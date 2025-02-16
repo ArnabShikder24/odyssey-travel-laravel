7e7993935ff0c7f3c1790d76373def7e2e46ae49
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-02-16 13:14:47 +0600

    check role

---
+++ app/Models/Auth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Auth extends Model
+{
+    use HasFactory;
+
+    protected $table = 'auth';
+
+    protected $fillable = ['username', 'email', 'role_id'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+}
+
